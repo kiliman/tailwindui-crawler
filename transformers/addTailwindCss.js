d30857f90e09fbf67b3a6869f87831dd466baaf1
kiliman <kiliman@gmail.com>
2020-04-18 21:56:07 -0400

    🚑 Hotfix invalid handling of ADDTAILWINDCSS_URL setting

--- transformers/addTailwindCss.js
+++ transformers/addTailwindCss.js
@@ -1,12 +1,12 @@
 const meta =
   '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
-const tui =
+const url =
   process.env.ADDTAILWINDCSS_URL ||
-  '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css">'
+  'https://cdn.jsdelivr.net/npm/@tailwindcss/ui@latest/dist/tailwind-ui.min.css'
 
 module.exports = function($) {
   // add stylesheets to <head/>
   $('head')
     .append(meta)
-    .append(tui)
+    .append(`<link rel="stylesheet" href="${url}">`)
 }
